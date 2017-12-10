dcf57e6a63ae309455559aeada9fee1c275b9c34
chicktech11 <chicktechies+11@gmail.com>
2017-12-10 11:06:43 -0800

    my custom first step

--- app/src/main/java/com/example/hlupico/mememaker/MainActivity.java
+++ app/src/main/java/com/example/hlupico/mememaker/MainActivity.java
@@ -1,7 +1,11 @@
 package com.example.hlupico.mememaker;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
     @Override
@@ -9,5 +13,21 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         getSupportActionBar().setElevation(0f);  /** Step 1 - remove automatic action bar shadow */
+        ImageButton imageButton =(ImageButton) findViewById(R.id.new_meme_button);
+        View.OnClickListener imageClickListener = getImageButtonOnClick();
+        imageButton.setOnClickListener(imageClickListener);
+    }
+    private View.OnClickListener getImageButtonOnClick(){
+        View.OnClickListener clickListener = new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(MainActivity.this, "You clicked the button!", Toast.LENGTH_SHORT).show();
+
+                Intent intent = new Intent(MainActivity.this, NewMemeActivity.class);
+                startActivity(intent);
+            }
+        };
+
+        return clickListener;
     }
 }
